f28e953ef0941717e750319623600afa2d85c6b8
MawranMashra <47461193+MarwanMashra@users.noreply.github.com>
2023-02-18 12:34:11 +0100

    XGBoostQ

---
+++ skopt2/learning/old_xgboost.py
@@ -0,0 +1,99 @@
+import numpy as np
+
+# from xgboost import XGBRegressor as _sk_XGBRegressor
+from sklearn.ensemble import GradientBoostingRegressor as _sk_XGBRegressor
+
+
+def _return_std(X, trees, predictions, min_variance):
+    # This derives std(y | x) as described in 4.3.2 of arXiv:1211.0906
+    std = np.zeros(len(X))
+
+    for tree in trees:
+        var_tree = tree.tree_.impurity[tree.apply(X)]
+
+        # This rounding off is done in accordance with the
+        # adjustment done in section 4.3.3
+        # of http://arxiv.org/pdf/1211.0906v2.pdf to account
+        # for cases such as leaves with 1 sample in which there
+        # is zero variance.
+        var_tree[var_tree < min_variance] = min_variance
+        mean_tree = tree.predict(X)
+        std += var_tree + mean_tree**2
+
+    std /= len(trees)
+    std -= predictions**2.0
+    std[std < 0.0] = 0.0
+    std = std**0.5
+    return std
+
+
+class XGBRegressor(_sk_XGBRegressor):
+    def __init__(
+        self,
+        max_depth=3,
+        learning_rate=0.1,
+        n_estimators=300,
+        subsample=1,
+        random_state=0,
+        # max_depth=3,
+        # learning_rate=0.1,
+        # n_estimators=300,
+        # silent=True,
+        # objective="binary:logistic",
+        # booster="gbtree",
+        # n_jobs=-1,
+        # nthread=None,
+        # gamma=0,
+        # min_child_weight=1,
+        # max_delta_step=0,
+        # subsample=1,
+        # colsample_bytree=1,
+        # colsample_bylevel=1,
+        # reg_alpha=0,
+        # reg_lambda=1,
+        # scale_pos_weight=1,
+        # base_score=0.5,
+        # random_state=0,
+        # seed=None,
+        # missing=None,
+        # min_variance=0.0,
+    ):
+        # self.min_variance = min_variance
+        super(XGBRegressor, self).__init__(
+            max_depth=max_depth,
+            learning_rate=learning_rate,
+            n_estimators=n_estimators,
+            subsample=subsample,
+            random_state=random_state,
+            # max_depth=max_depth,
+            # learning_rate=learning_rate,
+            # n_estimators=n_estimators,
+            # silent=silent,
+            # objective=objective,
+            # booster=booster,
+            # n_jobs=n_jobs,
+            # nthread=nthread,
+            # gamma=gamma,
+            # min_child_weight=min_child_weight,
+            # max_delta_step=max_delta_step,
+            # subsample=subsample,
+            # colsample_bytree=colsample_bytree,
+            # colsample_bylevel=colsample_bylevel,
+            # reg_alpha=reg_alpha,
+            # reg_lambda=reg_lambda,
+            # scale_pos_weight=scale_pos_weight,
+            # base_score=base_score,
+            # random_state=random_state,
+            # seed=seed,
+            # missing=missing,
+            # min_variance=min_variance,
+        )
+
+    def predict(self, X, return_std=False):
+        mean = super(XGBRegressor, self).predict(X)
+
+        if return_std:
+            std = _return_std(X, self.estimators_, mean, self.min_variance)
+            return mean, std
+
+        return mean
--- skopt2/learning/xgboost.py
+++ skopt2/learning/xgboost.py
@@ -1,99 +1,185 @@
 import numpy as np
 
-# from xgboost import XGBRegressor as _sk_XGBRegressor
-from sklearn.ensemble import GradientBoostingRegressor as _sk_XGBRegressor
+from xgboost import XGBRegressor as _xgb_XGBRegressor
+from functools import partial
+from sklearn.utils import check_random_state
+from sklearn.base import clone
+from joblib import Parallel, delayed
 
 
-def _return_std(X, trees, predictions, min_variance):
-    # This derives std(y | x) as described in 4.3.2 of arXiv:1211.0906
-    std = np.zeros(len(X))
-
-    for tree in trees:
-        var_tree = tree.tree_.impurity[tree.apply(X)]
-
-        # This rounding off is done in accordance with the
-        # adjustment done in section 4.3.3
-        # of http://arxiv.org/pdf/1211.0906v2.pdf to account
-        # for cases such as leaves with 1 sample in which there
-        # is zero variance.
-        var_tree[var_tree < min_variance] = min_variance
-        mean_tree = tree.predict(X)
-        std += var_tree + mean_tree**2
-
-    std /= len(trees)
-    std -= predictions**2.0
-    std[std < 0.0] = 0.0
-    std = std**0.5
-    return std
+def _parallel_fit(regressor, X, y):
+    return regressor.fit(X, y)
 
 
-class XGBRegressor(_sk_XGBRegressor):
+class XGBRegressor(_xgb_XGBRegressor):
     def __init__(
         self,
-        max_depth=3,
+        quantiles=[0.16, 0.5, 0.84],
+        quant_delta=1.0,
+        quant_thres=1.0,
+        quant_var=1.0,
+        base_score=0.5,
+        booster="gbtree",
+        colsample_bylevel=1,
+        colsample_bytree=1,
+        gamma=0,
         learning_rate=0.1,
-        n_estimators=300,
+        max_delta_step=0,
+        max_depth=3,
+        min_child_weight=1,
+        missing=None,
+        n_estimators=100,
+        n_jobs=-1,
+        nthread=None,
+        objective="reg:linear",
+        random_state=None,
+        reg_alpha=0,
+        reg_lambda=1,
+        scale_pos_weight=1,
+        seed=None,
+        silent=True,
         subsample=1,
-        random_state=0,
-        # max_depth=3,
-        # learning_rate=0.1,
-        # n_estimators=300,
-        # silent=True,
-        # objective="binary:logistic",
-        # booster="gbtree",
-        # n_jobs=-1,
-        # nthread=None,
-        # gamma=0,
-        # min_child_weight=1,
-        # max_delta_step=0,
-        # subsample=1,
-        # colsample_bytree=1,
-        # colsample_bylevel=1,
-        # reg_alpha=0,
-        # reg_lambda=1,
-        # scale_pos_weight=1,
-        # base_score=0.5,
-        # random_state=0,
-        # seed=None,
-        # missing=None,
-        # min_variance=0.0,
     ):
-        # self.min_variance = min_variance
-        super(XGBRegressor, self).__init__(
-            max_depth=max_depth,
+        self.quantiles = quantiles
+        self.quant_delta = quant_delta
+        self.quant_thres = quant_thres
+        self.quant_var = quant_var
+        self.random_state = random_state
+
+        super().__init__(
+            base_score=base_score,
+            booster=booster,
+            colsample_bylevel=colsample_bylevel,
+            colsample_bytree=colsample_bytree,
+            gamma=gamma,
             learning_rate=learning_rate,
+            max_delta_step=max_delta_step,
+            max_depth=max_depth,
+            min_child_weight=min_child_weight,
+            missing=missing,
             n_estimators=n_estimators,
+            n_jobs=n_jobs,
+            nthread=nthread,
+            objective=objective,
+            reg_alpha=reg_alpha,
+            reg_lambda=reg_lambda,
+            scale_pos_weight=scale_pos_weight,
+            seed=seed,
+            silent=silent,
             subsample=subsample,
-            random_state=random_state,
-            # max_depth=max_depth,
-            # learning_rate=learning_rate,
-            # n_estimators=n_estimators,
-            # silent=silent,
-            # objective=objective,
-            # booster=booster,
-            # n_jobs=n_jobs,
-            # nthread=nthread,
-            # gamma=gamma,
-            # min_child_weight=min_child_weight,
-            # max_delta_step=max_delta_step,
-            # subsample=subsample,
-            # colsample_bytree=colsample_bytree,
-            # colsample_bylevel=colsample_bylevel,
-            # reg_alpha=reg_alpha,
-            # reg_lambda=reg_lambda,
-            # scale_pos_weight=scale_pos_weight,
-            # base_score=base_score,
-            # random_state=random_state,
-            # seed=seed,
-            # missing=missing,
-            # min_variance=min_variance,
         )
 
-    def predict(self, X, return_std=False):
-        mean = super(XGBRegressor, self).predict(X)
+    def fit(self, X, y):
 
-        if return_std:
-            std = _return_std(X, self.estimators_, mean, self.min_variance)
-            return mean, std
+        rng = check_random_state(self.random_state)
 
-        return mean
+        super().set_params(random_state=rng)
+
+        regressors = []
+        for q in self.quantiles:
+            regressor = clone(super())
+            regressor.set_params(
+                objective=partial(
+                    XGBRegressor.quantile_loss,
+                    alpha=q,
+                    delta=self.quant_delta,
+                    threshold=self.quant_thres,
+                    var=self.quant_var,
+                )
+            )
+            regressors.append(regressor)
+
+        self.regressors_ = Parallel(n_jobs=self.n_jobs, backend="threading")(
+            delayed(_parallel_fit)(regressor, X, y) for regressor in regressors
+        )
+
+        return self
+
+    def predict(self, X, return_std=False, return_quantiles=False):
+        """Predict.
+
+        Predict `X` at every quantile if `return_std` is set to False.
+        If `return_std` is set to True, then return the mean
+        and the predicted standard deviation, which is approximated as
+        the (0.84th quantile - 0.16th quantile) divided by 2.0
+
+        Parameters
+        ----------
+        X : array-like, shape=(n_samples, n_features)
+            where `n_samples` is the number of samples
+            and `n_features` is the number of features.
+        """
+        predicted_quantiles = np.asarray([rgr.predict(X) for rgr in self.regressors_])
+        if return_quantiles:
+            return predicted_quantiles.T
+
+        elif return_std:
+            std_quantiles = [0.16, 0.5, 0.84]
+            is_present_mask = np.in1d(std_quantiles, self.quantiles)
+            if not np.all(is_present_mask):
+                raise ValueError(
+                    "return_std works only if the quantiles during "
+                    "instantiation include 0.16, 0.5 and 0.84"
+                )
+            low = self.regressors_[self.quantiles.index(0.16)].predict(X)
+            high = self.regressors_[self.quantiles.index(0.84)].predict(X)
+            mean = self.regressors_[self.quantiles.index(0.5)].predict(X)
+            return mean, ((high - low) / 2.0)
+
+        # return the mean
+        return self.regressors_[self.quantiles.index(0.5)].predict(X)
+
+    @staticmethod
+    def quantile_loss(y_true, y_pred, alpha, delta, threshold, var):
+        x = y_true - y_pred
+        grad = (
+            (x < (alpha - 1.0) * delta) * (1.0 - alpha)
+            - ((x >= (alpha - 1.0) * delta) & (x < alpha * delta)) * x / delta
+            - alpha * (x > alpha * delta)
+        )
+        hess = ((x >= (alpha - 1.0) * delta) & (x < alpha * delta)) / delta
+
+        grad = (np.abs(x) < threshold) * grad - (np.abs(x) >= threshold) * (
+            2 * np.random.randint(2, size=len(y_true)) - 1.0
+        ) * var
+        hess = (np.abs(x) < threshold) * hess + (np.abs(x) >= threshold)
+        return grad, hess
+
+    # @staticmethod
+    # def original_quantile_loss(y_true, y_pred, alpha, delta):
+    #     x = y_true - y_pred
+    #     grad = (
+    #         (x < (alpha - 1.0) * delta) * (1.0 - alpha)
+    #         - ((x >= (alpha - 1.0) * delta) & (x < alpha * delta)) * x / delta
+    #         - alpha * (x > alpha * delta)
+    #     )
+    #     hess = ((x >= (alpha - 1.0) * delta) & (x < alpha * delta)) / delta
+    #     return grad, hess
+
+    # def score(self, X, y):
+    #     y_pred = super().predict(X)
+    #     score = XGBRegressor.quantile_score(y, y_pred, self.quant_alpha)
+    #     score = 1.0 / score
+    #     return score
+
+    # @staticmethod
+    # def quantile_score(y_true, y_pred, alpha):
+    #     score = XGBRegressor.quantile_cost(x=y_true - y_pred, alpha=alpha)
+    #     score = np.sum(score)
+    #     return score
+
+    # @staticmethod
+    # def quantile_cost(x, alpha):
+    #     return (alpha - 1.0) * x * (x < 0) + alpha * x * (x >= 0)
+
+    # @staticmethod
+    # def get_split_gain(gradient, hessian, l=1):
+    #     split_gain = list()
+    #     for i in range(gradient.shape[0]):
+    #         split_gain.append(
+    #             np.sum(gradient[:i]) / (np.sum(hessian[:i]) + l)
+    #             + np.sum(gradient[i:]) / (np.sum(hessian[i:]) + l)
+    #             - np.sum(gradient) / (np.sum(hessian) + l)
+    #         )
+
+    #     return np.array(split_gain)
